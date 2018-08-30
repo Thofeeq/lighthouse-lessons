46eb915855e0cb31d205e8399466a7364eff32a9
Thofeeq <thofeeqm@gmail.com>
2018-08-30 20:44:55 +0000

    making changes to sort

--- w1d4/sort.js
+++ w1d4/sort.js
@@ -1,8 +1,8 @@
 var students = [
   { id: 1, name: "bruce",    age: 40 },
   { id: 2, name: "zoidberg", age: 22 },
-  { id: 3, name: "alex",     age: 22 },
-  { id: 4, name: "alex",     age: 30 }
+  { id: 3, name: "alex",     age: 30 },
+  { id: 4, name: "alex",     age: 22 }
 ];
 
 students.sort(function(a,b)
@@ -11,7 +11,7 @@ students.sort(function(a,b)
     {
       if(a.age > b.age)
       {
-        return 1;
+        return -1;
       }
       else if (a.age === b.age)
       {
@@ -19,7 +19,7 @@ students.sort(function(a,b)
       }
       else
       {
-        return -1;
+        return 1;
       }
 
       return 0;
